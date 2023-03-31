bfc8fc0fa7cc2e5fc991781754f76e8cd5d5f108
M4dn4ss <sura0046@hotmail.com>
2023-03-31 09:41:02 +0300

    router links

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,33 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { UsersComponent } from './users/users.component';
+import { CategoriesComponent } from './categories/categories.component';
+import { ProductsComponent } from './products/products.component';
+import { RouterModule, Routes } from '@angular/router';
+import { NotfoundComponent } from './notfound/notfound.component';
+
+const appRoutes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'home', component: HomeComponent },
+  { path: 'products', component: ProductsComponent },
+  { path: 'users', component: UsersComponent },
+  { path: '**', component: NotfoundComponent },
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    UsersComponent,
+    CategoriesComponent,
+    ProductsComponent,
+    NotfoundComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/products/products.component.ts
@@ -0,0 +1,29 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+
+@Component({
+  selector: 'app-products',
+  templateUrl: './products.component.html',
+  styleUrls: ['./products.component.css']
+})
+export class ProductsComponent implements OnInit {
+  products = [
+    {name: 'product 1'},
+    {name: 'product 2'},
+    {name: 'product 3'}
+  ]  
+
+  constructor (
+    private router: Router,
+    private route: ActivatedRoute  
+    ) {}
+
+  ngOnInit(): void {
+    
+  }
+
+  loadProducts() {
+    this.router.navigate(['products'],{ relativeTo: this.route });
+  }
+
+}
