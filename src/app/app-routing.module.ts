baf0932b89498f32d962a3ab17d94a23e3b02a8c
M4dn4ss <sura0046@hotmail.com>
2023-04-01 11:03:15 +0300

    child routes

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,34 @@
+import { NgModule } from "@angular/core";
+import { HomeComponent } from "./home/home.component";
+import { Routes, RouterModule } from "@angular/router";
+import { NotfoundComponent } from "./notfound/notfound.component";
+import { EditProductComponent } from "./products/edit-product/edit-product.component";
+import { ProductComponent } from "./products/product/product.component";
+import { ProductsComponent } from "./products/products.component";
+import { UserComponent } from "./users/user/user.component";
+import { UsersComponent } from "./users/users.component";
+
+
+const appRoutes: Routes = [
+    { path: '', component: HomeComponent },
+    { path: 'home', component: HomeComponent },
+    { path: 'products', component: ProductsComponent, children: [
+      { path: ':id', component: ProductComponent },
+      { path: ':id/edit', component: EditProductComponent }
+    ]},  
+    { path: 'users', component: UsersComponent, children: [
+      { path: ':name', component: UserComponent }
+    ]},  
+    { path: '**', component: NotfoundComponent },
+];
+
+@NgModule({
+    imports: [
+        RouterModule.forRoot(appRoutes)
+    ],
+    exports: [RouterModule]
+})
+
+export class AppRoutingModule {
+
+}
